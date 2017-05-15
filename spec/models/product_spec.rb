1056d7b232ea25c43b36049b30bdd6af2f7d3089
Shawn Yates <yates.shawnp@gmail.com>
2017-05-15 20:52:30 +0000

    test validations for products

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    before :each do
+      @category = Category.new(id: 1, name: "trucks")
+      @product = @category.products.new(name: "Toyota Tundra",
+                                        price_cents: 55000000,
+                                        quantity: 4)
+    end                                 
+    it "should succeed if product if all values are provided" do
+      expect(@product.valid?).to be true
+    end
+    it "should validate presense of name" do
+      @product.name = nil
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+    it "should validate presence of price" do
+      @product.price_cents = nil
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+    it "should validate presence of quantity" do
+      @product.quantity = nil
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+    it "should validate presence of category" do
+      product = Product.new(name:"candy", price_cents: 100, quantity: 5)
+      product.valid?
+      expect(product.errors.full_messages).to include("Category can't be blank")
+    end
+  end
+end
