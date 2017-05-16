e51295777fa94f74fd939783aadeffac6eaa3a28
Shawn Yates <yates.shawnp@gmail.com>
2017-05-16 21:17:09 +0000

    test addition to cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCart", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "one item added to cart after 'Add' button clicked" do
+    visit "/"
+    first('article.product').find_link('Add').click
+    within(".navbar-right") { expect(page).to have_content("My Cart (1)") }
+  end
+end
